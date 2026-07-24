4c608c7dee3fcd87bab834fac172fef6c5a64eb3
pinkXpink00 <kyperu07@gmail.com>
2026-07-24 15:30:54 +0300

    change 1

--- type hints.py
+++ type hints.py
@@ -2,9 +2,10 @@ from typing import List, Optional
 
 def validate_prompt(prompt: str) -> bool:
     "проверяем промт на безопасность"
-    dangerous_keywords = ["injection","override"]
+    dangerous_keywords = ["Ignore","injection","override"]
     return not any(keyword in prompt.lower() for keyword in dangerous_keywords)
 
 
 print(validate_prompt("hello world"))
 print(validate_prompt("Ignore my instruction"))
+print(validate_prompt("Ignore my instruction override"))
