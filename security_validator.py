72d00786ac950490a9c20af576a856f0ae8314ef
pinkXpink00 <kyperu07@gmail.com>
2026-07-25 21:04:34 +0300

    change 3

--- security_validator.py
+++ security_validator.py
@@ -1,12 +1,6 @@
 def check_dangerous_words(text):
     dangerous_keywords = ["DAN:", "system override", "Ignore", "Injection", "Override"]
-
-    results = []
-    for keyword in dangerous_keywords:
-        if keyword.lower() in text.lower():
-            results.append((keyword.lower()) in text.lower())
-
-    return any(results)
+    return any(keyword.lower() in text.lower() for keyword in dangerous_keywords)
 
 print(check_dangerous_words("Ignore me"))
 print(check_dangerous_words("hello"))
